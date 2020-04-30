96f84609eadc1786e179759f8975660c8107edb6
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-30 22:58:35 +0300

    created a save method

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -14,6 +14,7 @@ public abstract class Animals {
     public static final String HEALTH_HEALTHY="healthy";
     public static final String HEALTH_ILL="ill";
     public static final String HEALTH_OKAY="okay";
+
     public static final String AGE_NEWBORN="newborn";
     public static final String AGE_YOUNG="young";
     public static final String AGE_ADULT="adult";
