bd67cedcc5219b2990a7121374b08b57c1694ed0
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-30 23:23:18 +0300

    Created a test for creating aninstance of a class

--- src/test/java/EndangeredAnimalsTest.java
+++ src/test/java/EndangeredAnimalsTest.java
@@ -1,4 +1,5 @@
 import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -6,6 +7,16 @@ public class EndangeredAnimalsTest {
 
     @Rule
     public DatabaseRule databaseRule=new DatabaseRule();
+
+    @Test
+    public void testInstanceOfEndangeredAnimalsClass_true(){
+        EndangeredAnimals testAnimal= setUpNewAnimal();
+        assertEquals(true,testAnimal instanceof EndangeredAnimals);
+    }
+
+    private EndangeredAnimals setUpNewAnimal() {
+        return new EndangeredAnimals("Albino Girrafe","healthy","young");
+    }
 
 
 }
