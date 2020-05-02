cd82f1da1419778f50f47f01cef72a8928c7e2df
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-02 09:06:34 +0300

    Created find and all methods

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,4 +1,8 @@
+import org.sql2o.Connection;
+
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.Objects;
 
 public class Sightings {
 
@@ -32,5 +36,61 @@ public class Sightings {
 
     public Timestamp getTime() {
         return time;
+    }
+
+    public static List<Sightings> all(){
+        try (Connection con =DB.sql2o.open()){
+            String sql=("SELECT * FROM sightings");
+            return con.createQuery(sql)
+                    .executeAndFetch(Sightings.class);
+
+        }
+    }
+
+    public static Sightings find(int id){
+        try (Connection con=DB.sql2o.open()){
+            String sql="SELECT * FROM sightings WHERE id=:id";
+            return con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Sightings.class);
+
+        }
+    }
+
+    public void save(){
+
+       if(this.animal_id==-1||this.location_id==-1||this.ranger_id==-1){
+           throw new IllegalArgumentException("All fields must be filled");
+       }
+        try (Connection con=DB.sql2o.open()){
+            String sql= "INSERT INTO sightings (animal_id,ranger_id,location_id,time) VALUES (:animal_id,ranger_id," +
+                    ":location_id,now()";
+            this.id=(int) con.createQuery(sql,true)
+                    .addParameter("animal_id",this.animal_id)
+                    .addParameter("ranger_id",this.ranger_id)
+                    .addParameter("location_id",this.location_id)
+                    .executeUpdate()
+                    .getKey();
+
+
+            }
+
+        }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sightings sightings = (Sightings) o;
+        return id == sightings.id &&
+                location_id == sightings.location_id &&
+                ranger_id == sightings.ranger_id &&
+                animal_id == sightings.animal_id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, location_id, ranger_id, animal_id);
     }
 }
