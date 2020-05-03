d1dfcadc9c37359d384d62fc00601afec7208b32
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-03 09:13:37 +0300

    Updatedpom.xml and other files

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,9 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o( "jdbc:postgres://lwkrvliubcpfda:830bc954504472192aa7d9be1b7c1b4a1e5e06e0a1acb70d3507660c180bf5da@ec2-34-200-72-77.compute-1.amazonaws.com:5432/dbdm5s1n8hejg", "lwkrvliubcpfda", "830bc954504472192aa7d9be1b7c1b4a1e5e06e0a1acb70d3507660c180bf5da");
+    //use the line below if you want to run locally replacing user with db username and pass with db password
+//    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "wangui", "33234159");
+    // The line below runs app on heroku comment the line below if you want to run your app locally these are my
+    // credentials so they  will not work for you
+    public static Sql2o sql2o = new Sql2o( "jdbc:postgresql://ec2-34-200-72-77.compute-1.amazonaws.com:5432/dbdm5s1n8hejg", "lwkrvliubcpfda", "830bc954504472192aa7d9be1b7c1b4a1e5e06e0a1acb70d3507660c180bf5da");
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,7 +6,11 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     public void before(){
-        DB.sql2o=new Sql2o("jdbc:postgresql://tlafeaczpyudbw:cf9f6b81d98c18d5012a80842c01928b0331586444c6b9c08bc8504f67cbc678@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d7m5mfo9caefm6", "tlafeaczpyudbw", "cf9f6b81d98c18d5012a80842c01928b0331586444c6b9c08bc8504f67cbc678");
+        //use the line below if you want to run locally replacing user with your database username and pass with password
+//        DB.sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "wangui", "33234159");
+        // The line below runs app on heroku comment the line below if you want to run your app locally these are my
+        // credentials so they  will not work for you
+        DB.sql2o=new Sql2o("jdbc:postgresql://ec2-18-209-187-54.compute-1.amazonaws.com:5432/d7m5mfo9caefm6", "tlafeaczpyudbw", "cf9f6b81d98c18d5012a80842c01928b0331586444c6b9c08bc8504f67cbc678");
     }
 
     @Override
